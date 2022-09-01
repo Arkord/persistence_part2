faecc99f701063f1a7329348a5f7a827520b7e51
arkord <arkord1@gmail.com>
2022-09-01 15:53:09 -0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:persistence1/screens/home_screen.dart';
+
+void main() => runApp(const SqliteApp());
+
+class SqliteApp extends StatelessWidget {
+  const SqliteApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'SQLite Examle',
+      initialRoute: 'home',
+      routes: {
+        'home':(context) => const HomeScreen()
+      },
+      theme: ThemeData.light().copyWith(
+        appBarTheme: const AppBarTheme(
+          color: Color(0xFF38004D)
+        )
+      ),
+    );
+  }
+}
