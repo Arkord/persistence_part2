ff19706aba35037f0e923cacbb9a1c6919dba8cc
arkord <arkord1@gmail.com>
2022-09-02 12:57:15 -0500

    textformfields

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -20,45 +20,59 @@ class _HomeScreenState extends State<HomeScreen> {
         title: const Text("SQLite Database"),
         elevation: 0,
       ),
-      body: Stack(
-        children: [
-          TextFormField(controller: textControllerRace, decoration: const InputDecoration(
-            labelText: "Input the Race"
+      body: Container(
+        padding: const EdgeInsets.all(20.0),
+        child: ListView(
+          scrollDirection: Axis.vertical,
+          shrinkWrap: true,
+          children: [
+            TextFormField(controller: textControllerRace, decoration: const InputDecoration(
+              icon: Icon(Icons.view_comfortable),
+              labelText: "Input the Race"
    
-          ),),
-          TextFormField(controller: textControllerName, decoration: const InputDecoration(
-            labelText: "Input Name"
-            
-          ),),
-          Center (
-          child: (
-          // Container(
-          //   child: const Text('Main content'),
-          // )
-          FutureBuilder<List<Cat>>(
-            future: DatabaseHelper.instance.getCats(),
-            builder: (BuildContext context, AsyncSnapshot<List<Cat>> snapshot) {
-              if(!snapshot.hasData) {
-                return const Center(child:  Text('Loading...'));
-              }
-              else {
-                return snapshot.data!.isEmpty
-                ? const Center(child: Text('No cats in the list'))
-                : ListView(
-                  children: snapshot.data!.map((cat) {
-                    return Center(
-                      child: ListTile(
-                        title: Text(cat.name),
-                      ),
-                    );
-                  }).toList(),
-                );
-              }
-            },
-          )
+            ),),
+            TextFormField(controller: textControllerName, decoration: const InputDecoration(
+              icon: Icon(Icons.text_format_outlined),
+              labelText: "Input Name"
+              
+            ),),
+            Center (
+            child: (
+            // Container(
+            //   child: const Text('Main content'),
+            // )  
+            FutureBuilder<List<Cat>>(
+              future: DatabaseHelper.instance.getCats(),
+              builder: (BuildContext context, AsyncSnapshot<List<Cat>> snapshot) {
+                if(!snapshot.hasData) {
+                  return Center(
+                    child:  Container(
+                      padding: const EdgeInsets.only(top: 10),
+                      child: const Text('Loading...')
+                    )
+                  );
+                }
+                else {
+                  return snapshot.data!.isEmpty
+                  ? Center(child: Container(padding: const EdgeInsets.only(top: 10.0), child: const Text('No cats in the list')))
+                  : ListView(
+                    scrollDirection: Axis.vertical,
+                    shrinkWrap: true,
+                    children: snapshot.data!.map((cat) {
+                      return Center(
+                        child: ListTile(
+                          title: Text('Race: ${cat.race} | Name: ${cat.name} '),
+                        ),
+                      );
+                    }).toList(),
+                  );
+                }
+              },
+            )
+          ),
         ),
+        ]),
       ),
-      ]),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.save),
         onPressed: () async {
